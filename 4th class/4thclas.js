eb923a70e362ec3257a2b15512fbfdb9545b4351
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-07-23 00:08:56 +0500

    Create 4thclas.js

---
+++ 4th class/4thclas.js
@@ -0,0 +1,9 @@
+// var fullName = "subhanullah";
+// console.log(fullName);
+// fullName = "subhankaladi";
+
+// printed:subhanullah b/c js work top to bottom
+
+// const hello = "subhan"
+
+//  const subhan ="kaladi"
