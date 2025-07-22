27f4974ff513a2e769bef4aeafaff1d98c90afe7
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-07-22 23:58:23 +0500

    Create main.ts

---
+++ 00 to 10/5 nested object/main.ts
@@ -0,0 +1,20 @@
+
+
+type a ={
+    name : string,
+    date : {
+        day : number,
+        month : string,
+        year : number
+    },
+    age : number
+} 
+let b : a = {
+    name : "subhan",
+    date : {
+        day : 7,
+        month : "july",
+        year : 2024
+    },
+    age : 20
+}
