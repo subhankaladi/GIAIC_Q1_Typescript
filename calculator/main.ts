a2b2a9d5bee024cba4d93b77ed8a3a843a3348c6
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-07-23 00:09:00 +0500

    Create main.ts

---
+++ calculator/main.ts
@@ -0,0 +1,43 @@
+
+
+
+import inquirer from "inquirer"
+const answers = await inquirer.prompt([
+        {
+            type: "number",
+            name: "numberOne",
+            message: "enter your first number"
+        },
+        {
+            type: "number",
+            name: "numberTwo",
+            message: "enter your second number"
+        },
+        {
+            type: "list",
+            name: "operator",
+            choices: ["+", "-", "*", "/"],
+            message:"select operator"
+        }
+        
+    ]);
+const {numberOne, numberTwo, operator} = answers
+if (numberOne && numberTwo && operator) {
+    let result: number = 0
+
+    if (operator === "+") {
+        result = numberOne + numberTwo
+    }
+    if (operator === "-") {
+        result = numberOne - numberTwo
+    }
+    if (operator === "*") {
+        result = numberOne * numberTwo
+    }
+    if (operator === "/") {
+        result = numberOne / numberOne
+    }
+console.log("your result is" + result);
+}
+console.log(answers);
+
