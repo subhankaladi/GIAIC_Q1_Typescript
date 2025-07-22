ba49a5e9aeebb312063c850c911cd84de39b63cf
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-07-22 23:58:11 +0500

    Create main.ts

---
+++ 00 to 10/5 alised and anonymous type/main.ts
@@ -0,0 +1,24 @@
+
+
+// ALISED type 
+
+type a ={
+    name : string,
+    age : number
+}
+
+let b : a = {
+    name : "subhan",
+    age : 20
+}
+
+
+// ANONYMOUS type
+
+let c :{
+    name : string,
+    age : number
+} = {
+    name : "asad",
+    age : 29
+}
