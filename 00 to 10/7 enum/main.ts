a5e99fe9d8bc97ec5d585fdd67e21e5b867f5fb4
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-07-22 23:58:59 +0500

    Create main.ts

---
+++ 00 to 10/7 enum/main.ts
@@ -0,0 +1,22 @@
+
+
+enum sports {football , cricket , hockey}
+
+let sport : string  = sports[2]
+console.log(sport);
+
+
+
+
+enum sports2 {football , cricket , hockey}
+
+let sport2 : sports2 = sports2.cricket
+console.log(sport2);
+
+
+
+enum sports3 {football=3 , cricket=10 , hockey} // can assign value
+
+let sport3 : string  = sports3[11]
+console.log(sport3);
+
