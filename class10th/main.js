d95488386361fe5da35bedd20a0f707d648acc8f
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-07-23 00:09:07 +0500

    Create main.js

---
+++ class10th/main.js
@@ -0,0 +1,16 @@
+subhan();
+let b = "subhan";
+function subhan() {
+    console.log("SUBHAN KALADI");
+}
+console.log(a);
+var a;
+console.log(b);
+// let fruits = ["mango" , "banana" , "cherry" , "strawberry" , "peach"]
+// console.log( fruits.slice(1, 3));
+// let fruits: string[] = ["mango" , "banana" , "cherry" , "strawberry" , "peach"]
+// fruits.splice(1,0,"orange")
+// console.log( fruits);
+let fruits = ["mango", "banana", "cherry", "banana", "strawberry", "peach"];
+console.log(fruits.indexOf('banana', 2));
+export {};
