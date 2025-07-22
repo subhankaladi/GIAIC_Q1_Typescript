a0bc5d15aee5cb717b96f1418eca1b1a896db59a
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-07-22 23:59:07 +0500

    Create main.js

---
+++ 00 to 10/8 array/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// array declare ways
+let a = [];
+let b = [];
+let c = ["subhan", 15];
+c.push("kaladi)");
+console.log(c);
+function sk(name) {
+    console.log(name);
+}
+sk("Subhan");
