49021a4fe29dcb00681dd8c867d1e80e831e864d
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-07-22 23:59:12 +0500

    Create main.ts

---
+++ 00 to 10/8 array/main.ts
@@ -0,0 +1,22 @@
+
+
+// array declare ways
+
+
+
+let a : number[] = []
+
+let b : Array<number> = []
+
+let c = ["subhan" , 15]
+
+c.push("kaladi)")
+
+console.log(c);
+
+
+// function sk(name:string) {
+//     console.log(name)
+// }
+// sk("Subhan")
+
