2639a1ebb068bb9b9a340378f3d5937bc1f0eee8
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-07-22 23:57:40 +0500

    Create main.ts

---
+++ 00 to 10/1 strong type/main.ts
@@ -0,0 +1,21 @@
+
+// strong type
+
+let a : number = 15
+console.log(a);
+
+
+let b : string = "subhan"
+b = "kaladi"
+console.log(b);
+
+
+
+// inference
+
+let c = 10
+console.log(c);
+
+let d = "asad"
+d = "kaladi"
+console.log(d);
