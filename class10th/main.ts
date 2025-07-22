21eb499d1ce9f20778e2166aeedce0f73853e6fd
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-07-23 00:09:17 +0500

    Create main.ts

---
+++ class10th/main.ts
@@ -0,0 +1,53 @@
+import { log } from "console";
+
+
+
+subhan()
+
+
+let b = "subhan"
+
+function subhan() {
+    console.log("SUBHAN KALADI");
+    
+}
+
+
+console.log(a);
+
+var a;
+
+
+console.log(b);
+
+
+
+
+// let fruits = ["mango" , "banana" , "cherry" , "strawberry" , "peach"]
+
+// console.log( fruits.slice(1, 3));
+
+
+// let fruits: string[] = ["mango" , "banana" , "cherry" , "strawberry" , "peach"]
+
+// fruits.splice(1,0,"orange")
+// console.log( fruits);
+
+
+
+///////////////// INDEX OF  /////////////////
+
+
+// let fruits: string[] = ["mango" , "banana" , "cherry" , "banana" , "strawberry" , "peach"]
+
+// console.log(fruits.indexOf('banana' , 2));
+                            
+                           // 2nd Element index check
+                   
+
+
+// let fruits: string[] = ["mango" , "banana" , "cherry" , "banana" , "strawberry" , "peach"]
+
+// console.log(fruits.indexOf('banana' , 1));
+
+                       // first element index check
