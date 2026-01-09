2d35b95f7f9420c831604cb463d7fd259e005a2d
subhankaladi <subhankaladi@gmail.com>
2026-01-09 18:21:48 +0500

    Create main.js

---
+++ type & interface/main.js
@@ -0,0 +1,20 @@
+"use strict";
+///////////////////////////// TYPE ///////////////////
+let bonding = {
+    name: "subhan",
+    age: 20,
+    exp: true,
+    nickname: "SK",
+    day: "sunday",
+    health: true
+};
+console.log(bonding);
+let bondings = {
+    name: "ASAD",
+    age: 28,
+    exp: true,
+    nickname: "AK",
+    day: "sunday",
+    health: true
+};
+console.log(bondings);
